e10137a1326674277493b0ee01a9657dd9544fc1
Zaman Mughal <zamanmughal98@gmail.com>
2023-06-15 20:41:19 +0500

    created a separated file to handle the strings

---
+++ src/common/strings.js
@@ -0,0 +1,20 @@
+export const Strings = {
+  bannerImageText:
+    ' like to place an order for a succulent grilled chicken breast served with a zesty lemon garlic sauce and a side of perfectly seasoned roasted vegetables. To complement this exquisite dish, I would love a creamy mashed potato topped with melted butter and a sprinkle of fresh chives.For a refreshing beverage,',
+  orderNow: 'Order Now',
+  orderInitiated: 'Order initiated',
+  pageLoaded: 'page loaded',
+  internationalFlavorsDelivered:
+    'International flavors delivered to your office',
+  featuredProducts: 'Featured Products',
+  specialDish: 'Special Dish#',
+  quotationText:
+    'A food application is a mobile or web-based platform that provides users with a convenient and efficient way to explore, order, and enjoy a wide range of food options. These applications have revolutionized the food industry by bringing together restaurants, food vendors, and customers on a single platform.',
+  getYourOrder: 'Get Your Order In Minutes',
+  averageOrder: 'The Average Order takes',
+  minutesToDeliver: 'Minutes to be delivered',
+  whatPeoplesSaying: 'What People are Saying',
+  reviewText:
+    'Wow! I recently tried out this restaurant and I must say, it exceeded all my expectations. The ambiance was charming and inviting, creating a delightful dining experience.',
+  watchVideo: 'watch Video',
+};
